6da1415b75dffd830943a9ce24024ca57ae7d369
Sraleik <nicolas.rotier@gmail.com>
2017-06-05 15:24:18 +0200

    can detect claping

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 'use strict';
 
-const Hapi = require('hapi');
+console.log("Clap your hand")
 
-// Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({
-    port: 8080,
-    routes: {
-        cors: true
-    }
+// Require the module
+var clapDetector = require('clap-detector');
+
+// Define configuration
+var clapConfig = {
+   AUDIO_SOURCE: 'alsa hw:1,0',
+   DETECTION_PERCENTAGE_START : '5%', 
+   DETECTION_PERCENTAGE_END: '5%',  
+   CLAP_AMPLITUDE_THRESHOLD: 0.4, 
+   CLAP_ENERGY_THRESHOLD: 0.3,  
+   MAX_HISTORY_LENGTH: 10 
+};
+
+// Start clap detection
+clapDetector.start(clapConfig);
+
+// Register on clap event
+clapDetector.onClap(function(history) {
+    console.log('one clap !', history);
 });
-//Load plugins and start server
-server.register([
-    require('./routes/root')
-], (err) => {
 
-    if (err) {
-        throw err;
-    }
-
-    // Start the server
-    server.start((err) => {
-        if(err) {
-          console.log('registration failed', err)
-          return;
-        }
-        console.log('Server running at:', server.info.uri);
-    });
-
+// Register to a series of 3 claps occuring within 2 seconds
+clapDetector.onClaps(3, 2000, function(delay) {
+    console.log('3 clap !');
 });
+
+// Update the configuration
+//clapDetector.updateConfig({CLAP_ENERGY_THRESHOLD: 0.2});
+
