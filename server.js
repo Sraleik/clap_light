87fe271412747f22e5ac2cd158edbaf023b3e402
Sraleik <nicolas.rotier@gmail.com>
2017-06-07 17:08:33 +0200

    add hapi server and hello world to /

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 'use strict';
 
-console.log("Clap your hand")
-
-// Require the module
+const Hapi = require('hapi');
 var clapDetector = require('clap-detector');
 
 // Define configuration
 var clapConfig = {
    AUDIO_SOURCE: 'alsa hw:1,0',
-   DETECTION_PERCENTAGE_START : '5%', 
-   DETECTION_PERCENTAGE_END: '5%',  
-   CLAP_AMPLITUDE_THRESHOLD: 0.4, 
-   CLAP_ENERGY_THRESHOLD: 0.3,  
-   MAX_HISTORY_LENGTH: 10 
+   DETECTION_PERCENTAGE_START : '5%',
+   DETECTION_PERCENTAGE_END: '5%',
+   CLAP_AMPLITUDE_THRESHOLD: 0.4,
+   CLAP_ENERGY_THRESHOLD: 0.3,
+   MAX_HISTORY_LENGTH: 10
 };
 
 // Start clap detection
 clapDetector.start(clapConfig);
+
+console.log("Clap your hand")
 
 // Register on clap event
 clapDetector.onClap(function(history) {
@@ -30,4 +30,34 @@ clapDetector.onClaps(3, 2000, function(delay) {
 
 // Update the configuration
 //clapDetector.updateConfig({CLAP_ENERGY_THRESHOLD: 0.2});
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    port: 8080,
+    routes: {
+        cors: true
+    }
+});
+//Load plugins and start server
+server.register([
+    require('./routes/root')
+], (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    // Start the server
+    server.start((err) => {
+        if(err) {
+          console.log('registration failed', err)
+          return;
+        }
+        console.log('Server running at:', server.info.uri);
+    });
+
+});
+
+
 
